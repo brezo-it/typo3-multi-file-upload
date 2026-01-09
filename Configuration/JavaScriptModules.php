f888fd6b441996dce1e6d876f71e9a3e869c0043
Maik Preuss <maik.preuss@gmx.de>
2026-01-09 11:48:57 +0100

    Initial commit

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'dependencies' => ['form'],
+    'imports' => [
+        '@brezo-it/multi-file-upload/' => 'EXT:multi_file_upload/Resources/Public/JavaScript/',
+    ],
+];
